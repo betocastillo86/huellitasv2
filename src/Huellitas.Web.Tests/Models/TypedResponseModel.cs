8d4b63a88fbac0b747c456f354efe0e703cae36c
betocastillo86 <castillopradagabriel@gmail.com>
2020-04-20 19:42:22 -0500

    Fix publish for dotnet 3.1

---
+++ src/Huellitas.Web.Tests/Models/TypedResponseModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.Http;
+
+namespace Huellitas.Web.Tests.Models
+{
+    public class TypedResponseModel<T>
+    {
+        public T Content { get; set; }
+
+        public HttpResponseMessage Response { get; set; }
+    }
+}
